044671b81ffd624269c63ef9b1acc0409bd67c15
Yuriy Tkach <yuriy.tkach@schibsted.com>
2017-11-23 17:03:57 +0100

    Added creating of package-info if missing

---
+++ src/main/java/org/walkmod/findbugs/transformations/ExistingPackageInfoVisitor.java
@@ -0,0 +1,69 @@
+package org.walkmod.findbugs.transformations;
+
+import org.walkmod.javalang.ast.CompilationUnit;
+import org.walkmod.javalang.ast.ImportDeclaration;
+import org.walkmod.javalang.ast.PackageDeclaration;
+import org.walkmod.javalang.ast.expr.AnnotationExpr;
+import org.walkmod.javalang.ast.expr.MarkerAnnotationExpr;
+import org.walkmod.javalang.ast.expr.NameExpr;
+import org.walkmod.javalang.visitors.VoidVisitorAdapter;
+import org.walkmod.walkers.VisitorContext;
+
+import java.util.List;
+
+import static com.google.common.collect.ImmutableList.of;
+import static org.walkmod.findbugs.transformations.NotNullTransformation.ANNOTATION_NAME;
+import static org.walkmod.findbugs.transformations.NotNullTransformation.PACKAGE_NAME;
+
+public class ExistingPackageInfoVisitor extends VoidVisitorAdapter<VisitorContext> {
+
+  @Override
+  public void visit(final PackageDeclaration packageDeclaration, final VisitorContext ctx) {
+    final List<AnnotationExpr> annotations = packageDeclaration.getAnnotations();
+    if (annotations != null) {
+      boolean hasAnnotation = annotations.stream()
+          .map(AnnotationExpr::getName)
+          .map(NameExpr::toString)
+          .anyMatch(name -> name.equals(ANNOTATION_NAME));
+      if (!hasAnnotation) {
+        annotations.add(createAnnotationExpression());
+      }
+    } else {
+      packageDeclaration.setAnnotations(of(
+          createAnnotationExpression()
+      ));
+    }
+  }
+
+
+  @Override
+  public void visit(final CompilationUnit compilationUnit, final VisitorContext ctx) {
+    if (compilationUnit.getFileName().endsWith("package-info.java")) {
+      super.visit(compilationUnit, ctx);
+
+      final List<ImportDeclaration> imports = compilationUnit.getImports();
+      if (imports != null) {
+        boolean hasImport = imports.stream()
+            .map(ImportDeclaration::getName)
+            .map(NameExpr::toString)
+            .anyMatch(name -> name.equals(PACKAGE_NAME + ANNOTATION_NAME));
+        if (! hasImport) {
+          imports.add(createImportExpression());
+        }
+      } else {
+        compilationUnit.setImports(of(createImportExpression()));
+      }
+    }
+  }
+
+  private MarkerAnnotationExpr createAnnotationExpression() {
+    return new MarkerAnnotationExpr(new NameExpr(ANNOTATION_NAME));
+  }
+
+  private ImportDeclaration createImportExpression() {
+    return new ImportDeclaration(
+        new NameExpr(PACKAGE_NAME + ANNOTATION_NAME), false, false);
+  }
+
+
+}
--- src/test/java/org/walkmod/findbugs/transformations/NotNullTransformationTest.java
+++ src/test/java/org/walkmod/findbugs/transformations/NotNullTransformationTest.java
@@ -4,14 +4,18 @@ import org.junit.Test;
 import org.walkmod.javalang.ASTManager;
 import org.walkmod.javalang.ast.CompilationUnit;
 import org.walkmod.javalang.ast.ImportDeclaration;
+import org.walkmod.javalang.ast.Node;
 import org.walkmod.javalang.ast.expr.AnnotationExpr;
+import org.walkmod.javalang.test.SemanticTest;
+import org.walkmod.walkers.VisitorContext;
 
 import java.io.File;
+import java.util.Collection;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class NotNullTransformationTest {
+public class NotNullTransformationTest extends SemanticTest {
 
   @Test
   public void shouldAddAnnotationToPackageInfoWithOtherAnnotations() throws Exception {
@@ -22,21 +26,7 @@ public class NotNullTransformationTest {
     final NotNullTransformation visitor = new NotNullTransformation();
     visitor.visit(cu, null);
 
-    System.out.println(cu);
-
-    final List<ImportDeclaration> imports = cu.getImports();
-    assertThat(imports)
-        .hasSize(2)
-        .extracting(ImportDeclaration::getName)
-        .extracting("name")
-        .contains("com.schibsted.engprod.commons.annotations.NotNullByDefault");
-
-    final List<AnnotationExpr> annotations = cu.getPackage().getAnnotations();
-    assertThat(annotations)
-        .hasSize(2)
-        .extracting(AnnotationExpr::getName)
-        .extracting("name")
-        .contains("NotNullByDefault");
+    verifyAnnotationPresent(cu, 2);
   }
 
   @Test
@@ -48,21 +38,7 @@ public class NotNullTransformationTest {
     final NotNullTransformation visitor = new NotNullTransformation();
     visitor.visit(cu, null);
 
-    System.out.println(cu);
-
-    final List<ImportDeclaration> imports = cu.getImports();
-    assertThat(imports)
-        .hasSize(1)
-        .extracting(ImportDeclaration::getName)
-        .extracting("name")
-        .contains("com.schibsted.engprod.commons.annotations.NotNullByDefault");
-
-    final List<AnnotationExpr> annotations = cu.getPackage().getAnnotations();
-    assertThat(annotations)
-        .hasSize(1)
-        .extracting(AnnotationExpr::getName)
-        .extracting("name")
-        .contains("NotNullByDefault");
+    verifyAnnotationPresent(cu, 1);
   }
 
   @Test
@@ -74,18 +50,50 @@ public class NotNullTransformationTest {
     final NotNullTransformation visitor = new NotNullTransformation();
     visitor.visit(cu, null);
 
-    System.out.println(cu);
+    verifyAnnotationPresent(cu, 1);
+  }
 
+  @Test
+  public void shouldCreateAPackageInfoFileWhereNeeded() throws Exception {
+    final CompilationUnit cu = compile("package hello.world; class Hello { }");
+
+    final NotNullTransformation visitor = new NotNullTransformation();
+    final VisitorContext ctx = new VisitorContext();
+    visitor.visit(cu, ctx);
+
+    final Collection<Object> resultNodes = ctx.getResultNodes();
+    assertThat(resultNodes)
+        .hasSize(1)
+        .first()
+        .isOfAnyClassIn(CompilationUnit.class);
+    verifyAnnotationPresent((CompilationUnit)resultNodes.iterator().next(), 1);
+  }
+
+  @Test
+  public void shouldNotCreateAPackageInfoFileWhereExists() throws Exception {
+    final CompilationUnit cu = compile("package hello.world; class Hello { }",
+        "package hello; import java.lang.annotation.*; @Retention(RetentionPolicy.RUNTIME) public @interface NotNullByDefault { }",
+        "@NotNullByDefault package hello.world; import hello.NotNullByDefault;");
+
+    final NotNullTransformation visitor = new NotNullTransformation();
+    final VisitorContext ctx = new VisitorContext();
+    visitor.visit(cu, ctx);
+
+    final Collection<Object> resultNodes = ctx.getResultNodes();
+    assertThat(resultNodes).isEmpty();
+  }
+
+  private void verifyAnnotationPresent(final CompilationUnit cu, final int totalImportsSize) {
     final List<ImportDeclaration> imports = cu.getImports();
     assertThat(imports)
-        .hasSize(1)
+        .hasSize(totalImportsSize)
         .extracting(ImportDeclaration::getName)
-        .extracting("name")
-        .contains("com.schibsted.engprod.commons.annotations.NotNullByDefault");
+        .extracting(Node::toString)
+        .contains("hello.NotNullByDefault");
 
     final List<AnnotationExpr> annotations = cu.getPackage().getAnnotations();
     assertThat(annotations)
-        .hasSize(1)
+        .hasSize(totalImportsSize)
         .extracting(AnnotationExpr::getName)
         .extracting("name")
         .contains("NotNullByDefault");
