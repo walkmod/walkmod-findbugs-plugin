30ca15be17ac9f2339e7818ac0900cc13dbd6d28
Yuriy Tkach <yuriy.tkach@schibsted.com>
2017-11-23 12:44:48 +0100

    Added tests

--- src/test/java/org/walkmod/findbugs/transformations/NotNullTransformationTest.java
+++ src/test/java/org/walkmod/findbugs/transformations/NotNullTransformationTest.java
@@ -3,8 +3,13 @@ package org.walkmod.findbugs.transformations;
 import org.junit.Test;
 import org.walkmod.javalang.ASTManager;
 import org.walkmod.javalang.ast.CompilationUnit;
+import org.walkmod.javalang.ast.ImportDeclaration;
+import org.walkmod.javalang.ast.expr.AnnotationExpr;
 
 import java.io.File;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class NotNullTransformationTest {
 
@@ -19,9 +24,71 @@ public class NotNullTransformationTest {
 
     System.out.println(cu);
 
-//    MethodDeclaration md = (MethodDeclaration) cu.getTypes().get(0).getMembers().get(0);
-//    ReferenceType type = (ReferenceType) md.getParameters().get(0).getType();
-//    Assert.assertEquals(2, type.getArrayCount());
+    final List<ImportDeclaration> imports = cu.getImports();
+    assertThat(imports)
+        .hasSize(2)
+        .extracting(ImportDeclaration::getName)
+        .extracting("name")
+        .contains("com.schibsted.engprod.commons.annotations.NotNullByDefault");
+
+    final List<AnnotationExpr> annotations = cu.getPackage().getAnnotations();
+    assertThat(annotations)
+        .hasSize(2)
+        .extracting(AnnotationExpr::getName)
+        .extracting("name")
+        .contains("NotNullByDefault");
+  }
+
+  @Test
+  public void shouldAddAnnotationToEmptyPackageInfo() throws Exception {
+    final File packageInfoFile = new File(getClass().getClassLoader()
+        .getResource("package-info-empty/package-info.java").getFile());
+    final CompilationUnit cu = ASTManager.parse(packageInfoFile);
+
+    final NotNullTransformation visitor = new NotNullTransformation();
+    visitor.visit(cu, null);
+
+    System.out.println(cu);
+
+    final List<ImportDeclaration> imports = cu.getImports();
+    assertThat(imports)
+        .hasSize(1)
+        .extracting(ImportDeclaration::getName)
+        .extracting("name")
+        .contains("com.schibsted.engprod.commons.annotations.NotNullByDefault");
+
+    final List<AnnotationExpr> annotations = cu.getPackage().getAnnotations();
+    assertThat(annotations)
+        .hasSize(1)
+        .extracting(AnnotationExpr::getName)
+        .extracting("name")
+        .contains("NotNullByDefault");
+  }
+
+  @Test
+  public void shouldNotAddMoreAnnotationToPackageInfoThatAlreadyProcessed() throws Exception {
+    final File packageInfoFile = new File(getClass().getClassLoader()
+        .getResource("package-info-processed/package-info.java").getFile());
+    final CompilationUnit cu = ASTManager.parse(packageInfoFile);
+
+    final NotNullTransformation visitor = new NotNullTransformation();
+    visitor.visit(cu, null);
+
+    System.out.println(cu);
+
+    final List<ImportDeclaration> imports = cu.getImports();
+    assertThat(imports)
+        .hasSize(1)
+        .extracting(ImportDeclaration::getName)
+        .extracting("name")
+        .contains("com.schibsted.engprod.commons.annotations.NotNullByDefault");
+
+    final List<AnnotationExpr> annotations = cu.getPackage().getAnnotations();
+    assertThat(annotations)
+        .hasSize(1)
+        .extracting(AnnotationExpr::getName)
+        .extracting("name")
+        .contains("NotNullByDefault");
   }
 
 }
