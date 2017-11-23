900478dc7826f33564135e4cb07d494793cead6b
Carlos Ballesteros Velasco <carlos.ballesteros@schibsted.com>
2017-11-23 16:05:44 +0100

    Emit a warning when modifying a parameter

--- src/main/java/org/walkmod/findbugs/transformations/AddFinalKeywordVisitor.java
+++ src/main/java/org/walkmod/findbugs/transformations/AddFinalKeywordVisitor.java
@@ -1,5 +1,6 @@
 package org.walkmod.findbugs.transformations;
 
+import org.walkmod.javalang.ast.body.ClassOrInterfaceDeclaration;
 import org.walkmod.javalang.ast.body.MethodDeclaration;
 import org.walkmod.javalang.ast.body.ModifierSet;
 import org.walkmod.javalang.ast.body.Parameter;
@@ -11,10 +12,20 @@ import org.walkmod.walkers.VisitorContext;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.logging.Logger;
 
 public class AddFinalKeywordVisitor extends VoidVisitorAdapter<VisitorContext> {
+    static private Logger logger = Logger.getLogger(AddFinalKeywordVisitor.class.getName());
+
+    private String processingClass = "";
     final private ArrayList<Parameter> parameters = new ArrayList<>();
     final private HashSet<String> modifiedLocals = new HashSet<>();
+
+    @Override
+    public void visit(ClassOrInterfaceDeclaration n, VisitorContext arg) {
+        processingClass = n.getSymbolName();
+        super.visit(n, arg);
+    }
 
     @Override
     public void visit(MethodDeclaration n, VisitorContext arg) {
@@ -26,6 +37,13 @@ public class AddFinalKeywordVisitor extends VoidVisitorAdapter<VisitorContext> {
         for (Parameter param : parameters) {
             if (!modifiedLocals.contains(param.getSymbolName())) {
                 param.setModifiers(param.getModifiers() | ModifierSet.FINAL);
+            } else {
+                logger.warning("" +
+                        "Parameter '" + param.getSymbolName() + "'"
+                        + " in method '" + n.getSymbolName() + "'"
+                        + " and class '" + processingClass + "'" +
+                        " is being modified inside the function!"
+                );
             }
         }
     }
